e718e73f03586d480d4e645c41764784b4be8f33
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-01-28 21:30:01 +0200

    update jwt parser

--- src/Models/Users/Traits/Authentication/AuthenticationUsersTrait.php
+++ src/Models/Users/Traits/Authentication/AuthenticationUsersTrait.php
@@ -4,8 +4,14 @@ namespace ByTIC\Auth\Models\Users\Traits\Authentication;
 
 use ByTIC\Auth\Models\Users\Resolvers\UsersResolvers;
 use ByTIC\Auth\Models\Users\Traits\Authentication\AuthenticationUserTrait as User;
-use Lcobucci\JWT\Parser as JwtParser;
+use DateTimeZone;
+use Lcobucci\Clock\SystemClock;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token\Plain;
+use Lcobucci\JWT\Validation\Constraint\LooseValidAt;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
 use Nip\HelperBroker;
 use Nip_Helper_Passwords as PasswordsHelper;
 
@@ -26,22 +32,33 @@ trait AuthenticationUsersTrait
      */
     public function authenticateWithToken($tokenString, $key = null)
     {
-        $jwt = new JwtParser();
-
-        $token = $jwt->parse($tokenString);
         $key = $key ? $key : (app()->has('oauth.keys.public') ? app('oauth.keys.public') : null);
         if (empty($key)) {
             throw new \Exception("You need to define oauth keys in container [oauth.keys.public]");
         }
 
-        $signer = new Sha256();
-        $validJwt = $token->verify($signer, $key);
+        $jwtConfiguration = Configuration::forSymmetricSigner(
+            new Sha256(),
+            InMemory::plainText('')
+        );
 
-        if ($validJwt !== true) {
+        $jwtConfiguration->setValidationConstraints(
+            new LooseValidAt(new SystemClock(new DateTimeZone(\date_default_timezone_get()))),
+            new SignedWith(new Sha256(), InMemory::plainText($key))
+        );
+
+        $token = $jwtConfiguration->parser()->parse($tokenString);
+        if (!($token instanceof Plain)) {
             throw new \Exception("Invalid oauth Token");
         }
 
-        $entity = UsersResolvers::resolve($token->getClaim('sub'));
+        $constraints = $jwtConfiguration->validationConstraints();
+        if (!$jwtConfiguration->validator()->validate($token, ...$constraints)) {
+            throw new \Exception('No way!');
+        }
+
+        $claims = $token->claims();
+        $entity = UsersResolvers::resolve($claims->get('sub'));
         if (get_class($entity) != $this->getModel()) {
             throw new \Exception("Invalid user type in token");
         }
